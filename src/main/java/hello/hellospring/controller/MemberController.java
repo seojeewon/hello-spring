8c85176e7eb070bc1c997a4335a383a8ee795cfe
서지원 <readyjiwon@gmail.com>
2023-06-27 17:04:44 +0900

    [15강]컴포넌트 스캔과 자동 의존관계 설정
    @Component
    @Service
    @Repository

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+    private final MemberService memberService;
+    @Autowired  //spring container에서 memberService 가져와서 연결시켜줌->새로운 memberService가 계속 생성되지 않음
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
